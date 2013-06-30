c1c52ac4e48cec3d4e795402cba1be6d12e25337
saint11 <saintjust@gmail.com>
2013-06-30 06:43:00 -0300

    Properly added Monocle Engine

---
+++ Monocle/MonocleDemo/MonocleDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Monocle;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace MonocleDemo
+{
+    class MonocleDemo : Engine
+    {
+        public static Atlas Atlas;
+
+        static void Main(string[] args)
+        {
+            using (MonocleDemo demo = new MonocleDemo())
+            {
+                
+                demo.Run();
+            }
+        }
+
+        public MonocleDemo()
+            : base(1280, 720, 60, "Demo")
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            Atlas = new Atlas("Content/Graphics/graphics_1.xml", true);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
